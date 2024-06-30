e552682981c0703f13d8cda8d24ad956ade0d64e
Ceji-B <Ceji-B@users.noreply.github.com>
2024-06-30 14:08:59 +0800

    FEAT(Design): Vigan Ciry

--- pages/vigan-city/assets/js/scripts.js
+++ pages/vigan-city/assets/js/scripts.js
@@ -13,3 +13,14 @@ gsap.utils.toArray(".section").forEach((section, i) => {
   });
   
   
+  $(".custom-carousel").owlCarousel({
+    autoWidth: true,
+    loop: true
+  });
+  $(document).ready(function () {
+    $(".custom-carousel .item").click(function () {
+      $(".custom-carousel .item").not($(this)).removeClass("active");
+      $(this).toggleClass("active");
+    });
+  });
+  
